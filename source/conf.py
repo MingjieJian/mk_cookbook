13e0c329ff85d37416cdb096d24fc77dc6020d98
Mingjie Jian <jian-mingjie@outlook.com>
2023-11-17 13:53:51 +0100

    change to sphinx-book-theme

--- source/conf.py
+++ source/conf.py
@@ -27,12 +27,11 @@ source_suffix = {
 # -- Project information -----------------------------------------------------
 
 project = 'MK-cookbook'
-copyright = '2020, Mingjie Jian'
+copyright = '2023, Mingjie Jian'
 author = 'Mingjie Jian'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -63,7 +62,7 @@ exclude_patterns = ['*.ipynb']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx-book-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
